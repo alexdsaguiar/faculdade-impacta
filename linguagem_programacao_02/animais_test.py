f9b6bf1ffd23c7d04c08cd37bf90667311febdaa
Alexandre Aguiar <62570871+AlexAguiar89@users.noreply.github.com>
2020-09-10 18:30:16 -0300

    testando resultados da AC02

--- linguagem_programacao_02/animais_test.py
+++ linguagem_programacao_02/animais_test.py
@@ -1,8 +1,11 @@
 import unittest
 
 from animais import Gato
+from animais import Cobra
 
 gato_01 = Gato("nomeGato", "marron", 1, "pataGato")
+
+cobra_01 = Cobra("nomeCobra", "marrom", 1, True )
 
 print(gato_01.nome, gato_01.cor_pelo, gato_01.idade, gato_01.tipo_pata)
 print(gato_01.miar())
@@ -15,3 +18,5 @@ print(gato_01.morrer())
 print(gato_01.morrer())
 print(gato_01.morrer())
 print(gato_01.mamar())
+
+print(cobra_01.veneno)
