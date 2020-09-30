60dc0e1c5455519106c2a228a6e578562e60aa8a
Alexandre Aguiar <62570871+AlexAguiar89@users.noreply.github.com>
2020-09-30 14:11:42 -0300

    AC03 atualizada e finalizada

--- linguagem_programacao_02/teste.py
+++ linguagem_programacao_02/teste.py
@@ -1,14 +1,3 @@
-def eh_primo(n):
-    lista = []
-    if n < 2:
-        return False
-    else:
-        for div in range(2, n):
-            x = n % div
-            lista.append(x)
-            if n % div == 0:
-                return False
-                
-        return True
+from conta import Conta
 
-print(eh_primo(2))
+conta1 = Conta("bla",)
